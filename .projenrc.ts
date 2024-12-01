6be26eebe217b415710f6fc04125e65ed23b175b
Ayoub Umoru <ayoub.umoru@zerodotfive.com>
2024-12-01 17:54:27 +0100

    fix: fix projen issue

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,26 +1,37 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen";
+project.github?.actions.set(
+  "actions/upload-artifact",
+  "actions/upload-artifact@v4.3.6"
+);
 
-const PROJECT_NAME = 'cdk-aws-wafv2-geofence-lib';
+const PROJECT_NAME = "cdk-aws-wafv2-geofence-lib";
 const PROJECT_DESCRIPTION =
-  'The cdk-aws-wafv2-geofence-lib is an AWS CDK construct library that adds a AWS WAFv2 with GeoBlocking and AWS Managed Rules for AppSync, API Gateway or an ALB.';
+  "The cdk-aws-wafv2-geofence-lib is an AWS CDK construct library that adds a AWS WAFv2 with GeoBlocking and AWS Managed Rules for AppSync, API Gateway or an ALB.";
 
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'ZeroDotFive',
-  authorAddress: 'ayoub.umoru@zerodotfive.com',
-  cdkVersion: '2.127.0',
+  author: "ZeroDotFive",
+  authorAddress: "ayoub.umoru@zerodotfive.com",
+  cdkVersion: "2.127.0",
   majorVersion: 1,
-  defaultReleaseBranch: 'main',
+  defaultReleaseBranch: "main",
   authorOrganization: true,
-  jsiiVersion: '~5.3.0',
+  jsiiVersion: "~5.3.0",
   name: PROJECT_NAME,
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/ZDF-OSS/cdk-aws-wafv2-geofence-lib.git',
-  homepage: 'https://zerodotfive.com',
+  repositoryUrl: "https://github.com/ZDF-OSS/cdk-aws-wafv2-geofence-lib.git",
+  homepage: "https://zerodotfive.com",
   description: PROJECT_DESCRIPTION,
-  keywords: ['aws', 'cdk', 'awscdk', 'aws-cdk', 'wafv2', 'aws-waf', 'aws-wafv2', 'geoblock'],
-  gitignore: [
-    'cdk.out/',
+  keywords: [
+    "aws",
+    "cdk",
+    "awscdk",
+    "aws-cdk",
+    "wafv2",
+    "aws-waf",
+    "aws-wafv2",
+    "geoblock",
   ],
-  bundledDeps: ['openai'],
+  gitignore: ["cdk.out/"],
+  bundledDeps: ["openai"],
 });
 project.synth();
